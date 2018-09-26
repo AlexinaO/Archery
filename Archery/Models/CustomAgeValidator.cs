c91be0aba0219c7fd73daca3ceea62c7ca467e6d
AlexinaO <alexina.orsoni@gmail.com>
2018-09-27 00:23:42 +0200

    Ajout de CustomAgeValidator.cs --> Pas écrit

---
+++ Archery/Models/CustomAgeValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Models.user;
+
+namespace Archery.Models
+{
+    public class MinAgeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // Do some validation checks here
+            var result = new ValidationResult("Sorry you are not old enough");
+
+            return result;
+        }
+    }
