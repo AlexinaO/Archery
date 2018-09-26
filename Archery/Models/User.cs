56b022a287703ec1b8c1472c159acc6b11e7fee4
alexinaO <alexina.orsoni@gmail.com>
2018-09-26 15:44:37 +0200

    Ajout des Regex

--- Archery/Models/User.cs
+++ Archery/Models/User.cs
@@ -10,25 +10,33 @@ namespace Archery.Models
     {
         public int ID { get; set; }
 
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
         [Display(Name ="Nom")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
         [Display(Name = "Prénom")]
         public string LastName { get; set; }
 
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
         [Display(Name = "Mot de Passe")]
         [DataType(DataType.Password)]
+        [RegularExpression (@"^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\da-ZA-Z]).{6,}$", ErrorMessage ="{0} incorrect.")]
         public string Password { get; set; }
 
         [Display(Name ="Confirmation du mot de Passe")]
         [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="La confirmation n'est pas bonne.")]
         public string ConfirmedPassword { get; set; }
 
         [Required (ErrorMessage ="Le champ {0} est obligatoire")]
+        [StringLength(150, ErrorMessage ="Le champ {0} doit contenir {1} caractères max.")]
         [Display(Name ="Adresse E-mail")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Le format n'est pas bon.")]
         public string Mail { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
         [Display(Name ="Date de Naissance")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
