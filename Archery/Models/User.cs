e061ea0918eafb715cbc7c27f7c9620f27f1f9c1
alexinaO <alexina.orsoni@gmail.com>
2018-09-26 12:20:09 +0200

    Ajout des DataType sur User pour permettre aux helpers de mettre le bon type dans le input

--- Archery/Models/User.cs
+++ Archery/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,12 +14,14 @@ namespace Archery.Models
 
         public string LastName { get; set; }
 
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public string ConfirmedPassword { get; set; }
 
         public string Mail { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
     }
 }
