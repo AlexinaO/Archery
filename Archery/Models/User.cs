de8c6b12c5e5ef28f64112073312a4c0ce0d8953
alexinaO <alexina.orsoni@gmail.com>
2018-09-27 10:09:01 +0200

    Ajout de la customValidation Age sur BirthDate dans User.cs

--- Archery/Models/User.cs
+++ Archery/Models/User.cs
@@ -39,7 +39,7 @@ namespace Archery.Models
         [Required(ErrorMessage = "Le champ {0} est obligatoire")]
         [Display(Name ="Date de Naissance")]
         [DataType(DataType.Date)]
-        
+        [Age(9,ErrorMessage ="Vous devez avoir plus de 9 ans")]
         public DateTime BirthDate { get; set; }
     }
 }
