bc3a60293f840f457bee9fd83ad89472e61225d0
alexinaO <alexina.orsoni@gmail.com>
2018-09-27 17:06:28 +0200

    Ajout de la méthode d'enregistrement et d'un message que l'archer est bien inscrit

--- Archery/Controllers/ArchersController.cs
+++ Archery/Controllers/ArchersController.cs
@@ -1,4 +1,5 @@
-﻿using Archery.Models;
+﻿using Archery.Data;
+using Archery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,29 +16,29 @@ namespace Archery.Controllers
             return View();
         }
 
+
+        private ArcheryDbContext db = new ArcheryDbContext();
+
         [HttpPost]
         public ActionResult Subscribe(Archer archer )
         {
-            /* si on met ici alors cela va être uniquement dans ce contrôleur et il faudra le copier dans chaque contrôleur où on veut l'utiliser!*/
-            //if (DateTime.Now.AddYears(-9) <= archer.BirthDate) /*à utiliser pour 1ère et 2ème façon*/
-            //{
-            //    /*ViewBag.Erreur = "Vous êtes trop jeune !";
-            //    return View(); 1ère façon de faire*/
-            //    ModelState.AddModelError("BirthDate", "Date de naissance invalide");/*2ème façon de faire*/
-
-            //}
-
-
             if (ModelState.IsValid)
             {
-               
+                db.Archers.Add(archer);
+                db.SaveChanges();
+                TempData["Success"] = "Vous êtes bien inscrits";
+                //return View();
+                return RedirectToAction("index", "Home");
             }
             return View();
         }
 
-       //public static ErrorMessage (string errorMessage)
-       // {
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (!disposing)
+                this.db.Dispose();
+        }
 
-       // }
     }
 }
