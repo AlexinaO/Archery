af03642411917124f73dd146f4ecf44b69d61f8f
alexinaO <alexina.orsoni@gmail.com>
2018-09-27 15:44:32 +0200

    Création de la classe Configuration

---
+++ Archery/Migrations/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.Migrations;
+using Archery.Data;
+
+namespace Archery.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<ArcheryDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    }
+}
