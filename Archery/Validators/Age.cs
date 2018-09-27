f3ab71b19e8b656a5ef2942271b0d5478d696c18
alexinaO <alexina.orsoni@gmail.com>
2018-09-27 09:58:37 +0200

    Ajout du validateur pour l'âge

---
+++ Archery/Validators/Age.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Archery.Models
+{
+    public class Age : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime)
+            {
+                return DateTime.Now.AddYears(-9) <= (DateTime)value;
+            }
+            else
+                throw new ArgumentException("Le type doit être un DateTime");
+        }
+
+    }
+}
