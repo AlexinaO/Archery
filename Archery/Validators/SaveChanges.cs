8db8e5b2fba462e58f0240a5773265a50ed94f69
AlexinaO <alexina.orsoni@gmail.com>
2018-10-01 00:11:22 +0200

    Ajout de la classe SaveChanges pour gérer l'erreur après cryptage du mdp

---
+++ Archery/Validators/SaveChanges.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Web;
+
+namespace Archery.Validators
+{
+    [SerializableAttribute]
+    [SuppressMessageAttribute("Microsoft.Design", "CA1032:ImplementStandardExceptionConstructors", Justification = "SerializeObjectState used instead")]
+    public class SaveChanges : DataException
+    {
+
+    }
+}
