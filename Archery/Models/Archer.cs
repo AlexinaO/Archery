219766803b0be8366ed79ab03c4f939eca6ffee5
AlexinaO <alexina.orsoni@gmail.com>
2018-10-01 13:04:32 +0200

    Ajout de l'expression régulière pour le n° de licence

--- Archery/Models/Archer.cs
+++ Archery/Models/Archer.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Archery.Models
 {
-    public class Archer: User
+    public class Archer : User
     {
         [Required(ErrorMessage = "Le champ {0} est obligatoire")]
-        [Display (Name ="Numéro de licence")]
+        [Display(Name = "Numéro de licence")]
         [StringLength(15)]
+        [RegularExpression(@"^([0-9]{6})" + "([A-Z]{1})", ErrorMessage = "Le numéro de licence doit comporter 6 chiffres et terminer par une lettre majuscule")]
         public string LicenseNumber { get; set; }
-
     }
 }
