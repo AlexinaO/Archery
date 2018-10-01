f8a0c0e62ba9264ad89aaf19257edde0cc5963e7
AlexinaO <alexina.orsoni@gmail.com>
2018-10-01 17:06:46 +0200

    Création de la classe Bow

---
+++ Archery/Models/Bow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Archery.Models
+{
+    public class Bow : BaseModel
+    {
+        [Required]
+        [Display(Name = "Type d'arc")]
+        public string Name { get; set; }
+
+        [Display(Name = "Tournoi")]
+        public ICollection<Tournament> Tournaments { get; set; }
+    }
+}
