690f4a7e10a5d9802de0c8e596d901e4e8f34c88
AlexinaO <alexina.orsoni@gmail.com>
2018-10-01 17:06:57 +0200

    Création de la classe shooter

---
+++ Archery/Models/Shooter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Archery.Models
+{
+    public class Shooter : BaseModel
+    {
+        [Required]
+        [Display(Name = "Tournoi")]
+        public int TournamentID { get; set; }
+
+        [ForeignKey("TournamentID")]
+        public Tournament Tournament { get; set; }
+
+        [Required]
+        [Display(Name = "Arme")]
+        public int WeaponID { get; set; }
+
+        [ForeignKey("WeaponID")]
+        public Bow Bow { get; set; }
+
+        [Required]
+        [Display(Name = "Archer")]
+        public int ArcherID { get; set; }
+
+        [ForeignKey("ArcherID")]
+        public Archer Archer { get; set; }
+
+        [Display(Name = "Départ")]
+        public DateTime? Departure { get; set; }
+    }
+}
