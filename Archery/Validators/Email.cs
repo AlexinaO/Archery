504ca2c2a6356b31f913606b922ef5e9309d07cc
AlexinaO <alexina.orsoni@gmail.com>
2018-10-01 12:00:58 +0200

    Création du validateur Email pour vérifier qu'un archer ne puisse pas s'inscrire avec une adresse e-mail existant dans la bdd

---
+++ Archery/Validators/Email.cs
@@ -0,0 +1,17 @@
+﻿using Archery.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Archery.Validators
+{
+    public class Email : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            using (ArcheryDbContext db = new ArcheryDbContext())
+            {
+                return !db.Archers.Any(x => x.Mail == value.ToString());
+            }
+        }
+    }
+}
