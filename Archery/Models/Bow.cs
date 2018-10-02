91bb6395b2d7941e59f3ed7c05a0bc3ea1b89123
AlexinaO <alexina.orsoni@gmail.com>
2018-10-02 08:13:59 +0200

    Ajout de l'héritage BaseModel dans Bow

--- Archery/Models/Bow.cs
+++ Archery/Models/Bow.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Archery.Models
 {
-    public class Bow
+    public class Bow : BaseModel
     {
         [Required]
         [Display(Name = "Type d'arc")]
