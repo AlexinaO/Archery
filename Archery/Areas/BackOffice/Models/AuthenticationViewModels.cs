d84f6044d17632031a7d983b87e76e2e7e51051e
AlexinaO <alexina.orsoni@gmail.com>
2018-10-03 14:47:40 +0200

    Ajout des attributs

--- Archery/Areas/BackOffice/Models/AuthenticationViewModels.cs
+++ Archery/Areas/BackOffice/Models/AuthenticationViewModels.cs
@@ -1,6 +1,13 @@
-﻿namespace Archery.Areas.BackOffice.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Archery.Areas.BackOffice.Models
 {
     public class AuthenticationLoginViewModels
     {
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        public string Password { get; set; }
     }
 }
