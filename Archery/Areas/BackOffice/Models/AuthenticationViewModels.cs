257b001d04c105c3cc6bdf15e6b1c9a16a9cd470
AlexinaO <alexina.orsoni@gmail.com>
2018-10-03 15:32:43 +0200

    Ajout du DataType sur le Password pour le cacher

--- Archery/Areas/BackOffice/Models/AuthenticationViewModels.cs
+++ Archery/Areas/BackOffice/Models/AuthenticationViewModels.cs
@@ -4,10 +4,13 @@ namespace Archery.Areas.BackOffice.Models
 {
     public class AuthenticationLoginViewModels
     {
+        [Display(Name = "Login")]
         [Required(ErrorMessage = "Le champ {0} est obligatoire")]
         public string Mail { get; set; }
 
+        [Display(Name = "Mot de passe")]
         [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
