a6eb49056b6d85b0a54225e20cba1620c04ba65e
AlexinaO <alexina.orsoni@gmail.com>
2018-10-03 16:39:28 +0200

    Filtre avant session dans AuthenticationAttribute

---
+++ Archery/Filters/AuthenticationAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Archery.Filters
+{
+    public class AuthenticationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["ADMINISTRATOR"] == null)
+            {
+                filterContext.Result = new RedirectResult(@"\BackOffice\authentication\login");
+                base.OnActionExecuting(filterContext);
+            }
+        }
+    }
+}
