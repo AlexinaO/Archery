1823bab3d49adb3ca665881689d1ea4d4c9c332d
AlexinaO <alexina.orsoni@gmail.com>
2018-10-03 14:42:10 +0200

    Création du controller Authentication pour lequel la vue est Login

---
+++ Archery/Areas/BackOffice/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Archery.Areas.BackOffice.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        // GET: BackOffice/Authentication
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
