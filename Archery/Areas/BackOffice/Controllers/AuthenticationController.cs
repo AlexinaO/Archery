ee0458e770d7f489efb2ead6b7f3a27718f2ac55
AlexinaO <alexina.orsoni@gmail.com>
2018-10-03 15:32:14 +0200

    Ajout de la méthode HttpPost

--- Archery/Areas/BackOffice/Controllers/AuthenticationController.cs
+++ Archery/Areas/BackOffice/Controllers/AuthenticationController.cs
@@ -1,12 +1,38 @@
-﻿using System.Web.Mvc;
+﻿using Archery.Areas.BackOffice.Models;
+using Archery.Controllers;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Archery.Areas.BackOffice.Controllers
 {
-    public class AuthenticationController : Controller
+    public class AuthenticationController : BaseController
     {
         // GET: BackOffice/Authentication
         public ActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(AuthenticationLoginViewModels model)
+        {
+            if (ModelState.IsValid)
+            {
+                //var hash = model.Password.HashMD5();
+                //var admin = db.Administrators.SingleOrDefault(x => x.Mail == model.Mail && x.Password == hash);
+                var admin = db.Administrators.SingleOrDefault(x => x.Mail == model.Mail && x.Password == model.Password);
+
+                if (admin == null)
+                {
+                    ModelState.AddModelError("Mail", "Login / mot de passe invalide");
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Dashboard", new { area = "BackOffice" }); //area inutile ici puisque nous sommes déjà dedans mais cela n'affecte rien de la mettre quand même
+                }
+            }
             return View();
         }
     }
