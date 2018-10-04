4672506d9f2cc89622e806e900155884033ca83c
AlexinaO <alexina.orsoni@gmail.com>
2018-10-04 12:12:05 +0200

    Création du modèle TournamentPictures pour définir le modèle d'une image

---
+++ Archery/Models/TournamentPictures.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Archery.Models
+{
+    public class TournamentPictures
+    {
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string ContentType { get; set; }
+
+        [Required]
+        public byte[] Content { get; set; } //pour un enregistrement en dossier, c'est l'url
+
+        public int TournamentID { get; set; }
+
+        [ForeignKey("TournamentID")]
+        public Tournament Tournament { get; set; }
+    }
+}
