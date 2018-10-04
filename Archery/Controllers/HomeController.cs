085bbcf4a3b9d7be557085dd4950f489144af3ec
AlexinaO <alexina.orsoni@gmail.com>
2018-10-04 17:01:40 +0200

    Ajout d'une action Details dans le HomeController

--- Archery/Controllers/HomeController.cs
+++ Archery/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Archery.Models;
 using System;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 
 namespace Archery.Controllers
@@ -29,6 +30,21 @@ namespace Archery.Controllers
                 CreatedDate = DateTime.Now
             };
             return View(modelInfo);
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tournament tournament = db.Tournaments.Include("Bows").Include("Pictures").SingleOrDefault(x => x.ID == id);
+            if (tournament == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tournament);
         }
     }
 }
