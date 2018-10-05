6aa48db1fbab875de4047919f6f607d7a756152d
AlexinaO <alexina.orsoni@gmail.com>
2018-10-05 11:47:42 +0200

    Création du modèle AuthenticationUserViewModels dans la partie principale

---
+++ Archery/Models/AuthenticationViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Archery.Models
+{
+    public class AuthenticationUserViewModels
+    {
+        [Display(Name = "Login")]
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        public string Mail { get; set; }
+
+        [Display(Name = "Mot de passe")]
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
